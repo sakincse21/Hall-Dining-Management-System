ec0fae8646ef4ca5fc6b99d7420065572cf6f780
sakincse21 <saleheen.sakin@gmail.com>
2025-10-10 12:46:48 +0600

    summary health

--- backend/src/app/modules/summary/summary.route.ts
+++ backend/src/app/modules/summary/summary.route.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { getSummary, getSummaryByDate } from "./summary.controller";
 import { authCheck } from "../../middlewares/authUser";
 import { IROLE } from "../user/user.interface";
+import { Request, Response } from "express";
 
 const router = Router();
 
-
-router.post('/', authCheck(IROLE.admin), getSummary)
-router.get('/:date', getSummaryByDate)
+router.post("/", authCheck(IROLE.admin), getSummary);
+router.get("/health", (req: Request, res: Response) => {
+  res.send(`Summary route working fine`);
+});
+router.get("/:date", getSummaryByDate);
 
 export const SummaryRouter = router;
